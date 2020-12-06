a6cccbd4d299a7c401de3510fdbffde8d0503f6c
ojab <ojab@ojab.ru>
2020-12-06 17:19:03 +0000

    Add spec for `Watir::Rails::Middleware#pending_requests?`

--- spec/watir/rails/middleware_spec.rb
+++ spec/watir/rails/middleware_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe Watir::Rails::Middleware do
-  let(:app) { double('app') }
   let(:middleware) { described_class.new app }
 
   context '#call' do
+    let(:app) { ->(_env) {} }
+
     it '/__identify__ returns app id' do
       expect(app).not_to receive(:call)
       expect(middleware.call('PATH_INFO' => '/__identify__')).to eq([200, {}, [app.object_id.to_s]])
@@ -16,15 +17,33 @@ describe Watir::Rails::Middleware do
       middleware.call(env)
     end
 
-    it 'errors are stored and re-raised' do
-      error = RuntimeError.new
-      allow(app).to receive(:call).and_raise error
+    context 'when app is raising an error' do
+      let(:app) { ->(env) { raise env[:error] } }
+      let(:error) { RuntimeError.new('oops') }
 
-      expect {
-        middleware.call({})
-      }.to raise_error(error)
+      it 'errors are stored and re-raised' do
+        error = RuntimeError.new
 
-      expect(middleware.error).to eq(error)
+        expect { middleware.call(error: error) }.to raise_error(error)
+
+        expect(middleware.error).to be(error)
+      end
+    end
+  end
+
+  context '#pending_requests?' do
+    let(:app) { ->(_env) { sleep } }
+    let(:sleep_thread) { Thread.new { middleware.call({}) } }
+
+    it 'works' do
+      expect(middleware.pending_requests?).to be(false)
+
+      sleep_thread.join(0.1)
+      expect(middleware.pending_requests?).to be(true)
+
+      sleep_thread.kill
+      sleep_thread.join
+      expect(middleware.pending_requests?).to be(false)
     end
   end
 end
